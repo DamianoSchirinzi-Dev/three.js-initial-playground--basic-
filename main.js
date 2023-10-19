9e374cad5c291796845f01efb31cfe9cf73d604e
Damiano Schirinzi <damianoschirinzi.dev@gmail.com>
2023-10-19 19:32:00 +0100

    Second Commit

--- main.js
+++ main.js
@@ -1,41 +1,31 @@
-import * as THREE from "../node_modules/three/build/three.module.js"
+import * as THREE from "../node_modules/three/build/three.module.js";
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometery = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometery, material);
-scene.add(cube);
-cube.position.x = -2;
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth,
+  window.innerHeight,
+  1,
+  500
+);
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
 
-const geometery2 = new THREE.BoxGeometry(1, 1, 1);
-const material2 = new THREE.MeshBasicMaterial({ color: 0xFF0000});
-const cube2 = new THREE.Mesh(geometery2, material2);
-scene.add(cube2);
-cube2.position.y  += 2;
+const scene = new THREE.Scene();
 
-camera.position.z = 5;
+const material = new THREE.LineBasicMaterial({ color: 0x0000ff })
 
-function animate() {
-    requestAnimationFrame(animate);
-    
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+const points = [];
+points.push(new THREE.Vector3(-10, 0, 0));
+points.push(new THREE.Vector3(0, 10, 0));
+points.push(new THREE.Vector3(10, 0, 0));
 
-    cube2.position.x += 0.01;
-    cube2.rotation.x -= 0.01;
-    cube2.rotation.y -= 0.01;
+const geometry = new THREE.BuggerGeometry().setFromPoints(points);
 
-    renderer.render(scene, camera);
-}
+const line = new THREE.Line( geometry, material);
 
-animate();
+scene.add(line);
+renderer.render(scene, camera);
